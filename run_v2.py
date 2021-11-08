2609d78e4c991c7ebacdd92ce1b3bde0913f5323
Paul Janson <pauljanson002@gmail.com>
2021-11-08 20:11:15 +0530

    Done

--- run_v2.py
+++ run_v2.py
@@ -7,9 +7,10 @@ if __name__ == '__main__':
     parser.add_argument("--task_id",type=int, default=1)
     parser.add_argument("--config",type=str,default="test")
     parser.add_argument("--online",action="store_true")
-    f = open(f"config/{parser.config}.yaml", "r")
-    config = EasyDict(load(f, Loader=Loader))
     args = parser.parse_args()
+    f = open(f"config/{args.config}.yaml", "r")
+    config = EasyDict(load(f, Loader=Loader))
+
     command = f"python -u train.py --epochs {config.epochs} --project_name {config.project_name}  --batch_size {config.batch_size} --learning_rate {config.learning_rate} --task_id {args.task_id}"
     if args.online:
         command += " --online"
