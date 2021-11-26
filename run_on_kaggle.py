7ced2ef646a58fe051041601ba9b772c0cfbbd54
Paul Janson <pauljanson002@gmail.com>
2021-11-26 16:13:37 +0530

    Commit

--- run_on_kaggle.py
+++ run_on_kaggle.py
@@ -3,7 +3,7 @@ import os
 if __name__ == '__main__':
     os.system("git clone https://github.com/Pauljanson002/IncLearn.git")
     os.system("wandb login 5c39896b218f3aa477a66b1bd59cb4500e16a396")
-    os.system("cp -r ../input/inclearndata ./IncLearn/checkpoint")
+    #os.system("cp -r ../input/inclearndata ./IncLearn/checkpoint")
     os.chdir("./IncLearn")
     config = "ex_2"
     print(os.getcwd())
